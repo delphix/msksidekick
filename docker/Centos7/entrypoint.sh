d09735c2d1ac0e96284044ba1b2c49f3dd32c0d4
Ajay Thotangare <ajay.thotangare@delphix.com>
2021-09-23 21:52:08 -0500

    Test Centos7Build

---
+++ docker/Centos7/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /github/workspace
+ls -l
+python3 setup.py install
+pip3 install pyinstaller
+# for pyinstaller to run
+pip3 uninstall jeepney -y
+pip3 install jeepney
+pyinstaller --onefile --clean msksidekick.py
+cd /github/workspace/dist
+tail -f /dev/null
