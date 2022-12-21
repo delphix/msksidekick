56acd1aa0b285c394b78dd5cee9253a594d29426
Ajay <ajay.thotangare@delphix.com>
2022-12-20 22:21:08 -0600

    fix github workflows

--- docker/Centos7/entrypoint.sh
+++ docker/Centos7/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 cd /github/workspace
-ls -l
-python3 setup.py install
-pip3 install wheel
-pip3 install pyinstaller
+cd /github/workspace
+python3.8 setup.py install
+#pip3 install wheel
+#pip3 install pyinstaller
 # for pyinstaller to run
-pip3 uninstall jeepney -y
-pip3 install jeepney
-pyinstaller --onefile --clean msksidekick.py
+#pip3 uninstall jeepney -y
+#pip3 install jeepney
+# pyinstaller --onefile --clean msksidekick.py
+pyInstaller --onefile --clean --hidden-import pkg_resources msksidekick.py
 cd /github/workspace/dist
 # tail -f /dev/null
